6efeb8b7e85dbbae9705b08faa0f273eb8aa641a
nakamitsu_nozomi <exnnss0131@gmail.com>
2024-04-28 15:05:13 +0900

    Revert "chore: use biome"
    This reverts commit 5e812ffc084fa676325a201d308f6f56915a3088.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const element = document.getElementById('root');
-element
-  ? ReactDOM.createRoot(element).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
-  : null;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
