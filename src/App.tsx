1f30ba892be100d8a3f3f8206b48aaddfa7d37ee
nakamitsu_nozomi <exnnss0131@gmail.com>
2024-04-28 15:54:12 +0900

    chore: add stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import {useCallback, useState} from 'react';
-import viteLogo from '/vite.svg';
-import './App.css';
-import reactLogo from './assets/react.svg';
+import styles from './App.module.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = useCallback(() => {
-    setCount((count) => count + 1);
-  }, []);
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick} type="button">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer" className={styles.text}>
+        テスト
+      </a>
     </>
   );
-}
+};
 
 export default App;
