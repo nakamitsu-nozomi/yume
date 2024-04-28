a24daccf6e410651bdf76f1b30549b466e433032
nakamitsu_nozomi <exnnss0131@gmail.com>
2024-04-28 17:27:52 +0900

    chore: add @tanstack/react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 
-const element = document.getElementById('root');
-element
-  ? ReactDOM.createRoot(element).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
-  : null;
+// Import the generated route tree
+import { routeTree } from './routeTree.gen';
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root');
+if (rootElement != null) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite()],
 });
