49103067170a1ac1e98e80eabe0a5cab81ecf549
nakamitsu_nozomi <exnnss0131@gmail.com>
2024-04-29 12:43:53 +0900

    fix: Suspense

--- src/features/prefecture/components/PrefectureList/PrefectureList.tsx
+++ src/features/prefecture/components/PrefectureList/PrefectureList.tsx
@@ -1,27 +1,29 @@
-import { type ChangeEvent, type FC, Suspense } from 'react';
+import type { ChangeEvent, FC } from 'react';
 import { CheckBox } from '../../../../ui/Checkbox';
-import type { AggregatePrefecture } from '../../utils/prefecture.ts';
+import type { Prefecture } from '../../domain/entity.ts';
 import styles from './PrefectureList.module.css';
 
 interface Props {
-  aggregatePrefectures: AggregatePrefecture[];
+  prefectures: Prefecture[];
+  checkboxStates: { [prefCode: string]: boolean };
   onChecked: (e: ChangeEvent<HTMLInputElement>, prefCode: number) => void;
 }
 
-export const PrefectureList: FC<Props> = ({ aggregatePrefectures, onChecked }) => {
+export const PrefectureList: FC<Props> = ({ prefectures, checkboxStates, onChecked }) => {
   return (
-    <Suspense fallback={<p>読み込み中</p>}>
-      <div className={styles.prefectureList}>
-        {aggregatePrefectures.map((aggregatePrefecture) => (
+    <div className={styles.prefectureList}>
+      {prefectures.map((prefecture) => {
+        const value = checkboxStates[prefecture.prefCode] ? checkboxStates[prefecture.prefCode] : false;
+        return (
           <CheckBox
-            id={`${aggregatePrefecture.prefCode}`}
-            checked={aggregatePrefecture.isChecked}
-            label={aggregatePrefecture.prefName}
-            key={aggregatePrefecture.prefCode}
-            onChange={(e) => onChecked(e, aggregatePrefecture.prefCode)}
+            id={`${prefecture.prefCode}`}
+            checked={value}
+            label={prefecture.prefName}
+            key={prefecture.prefCode}
+            onChange={(e) => onChecked(e, prefecture.prefCode)}
           />
-        ))}
-      </div>
-    </Suspense>
+        );
+      })}
+    </div>
   );
 };
--- src/features/prefecture/pages/PrefecturePage.tsx
+++ src/features/prefecture/pages/PrefecturePage.tsx
@@ -1,108 +1,52 @@
-import { type ChangeEvent, type FC, useCallback, useEffect, useMemo, useState } from 'react';
+import { type ChangeEvent, type FC, Suspense, useCallback, useState } from 'react';
 import { PrefectureCharts, type Props } from '../components/PrefectureCharts';
 import { PrefectureList } from '../components/PrefectureList';
-import { useGetPopulationByPrefCodeSuspenseQuery, useGetPrefectureListSuspenseQuery } from '../domain/hooks.ts';
-import { type AggregatePrefecture, createAggregatePrefecture } from '../utils/prefecture.ts';
+import { useGetPrefectureListSuspenseQuery } from '../domain/hooks.ts';
 
-type PopulationCategory = '総人口' | '年少人口' | '生産年齢人口' | '老年人口';
+export type PopulationLabel = '総人口' | '年少人口' | '生産年齢人口' | '老年人口';
+
 export const PrefecturePage: FC<Props> = () => {
-  const [aggregatePrefectures, setAggregatePrefectures] = useState<AggregatePrefecture[]>([]);
+  const [checkboxStates, setCheckboxStates] = useState<{ [code: string]: boolean }>({});
   const getPrefectureListQuery = useGetPrefectureListSuspenseQuery();
-  const getPopulationByPrefCodeSuspenseQueries = useGetPopulationByPrefCodeSuspenseQuery(
-    aggregatePrefectures?.filter((aggregatePrefecture) => aggregatePrefecture.isChecked).map((v) => v.prefCode),
-    {
-      enabled: !getPrefectureListQuery.isLoading,
-    },
-  );
-  const dataKeys = aggregatePrefectures
-    .filter((aggregatePrefecture) => aggregatePrefecture.isChecked)
-    .map((v) => v.prefName);
+  const selectedPrefCodes = Object.keys(checkboxStates).map((v) => Number(v));
 
-  useEffect(() => {
-    if (getPrefectureListQuery.data != null) {
-      setAggregatePrefectures(getPrefectureListQuery.data.map((prefecture) => createAggregatePrefecture(prefecture)));
-    }
-  }, [getPrefectureListQuery.data]);
+  const dataKeys = getPrefectureListQuery.data
+    .filter((prefecture) => selectedPrefCodes.includes(prefecture.prefCode))
+    .map((v) => v.prefName);
 
   const handleChecked = useCallback(
     (e: ChangeEvent<HTMLInputElement>, prefCode: number) => {
-      setAggregatePrefectures(
-        aggregatePrefectures.map((aggregatePrefecture) => {
-          if (aggregatePrefecture.prefCode === prefCode) {
-            return {
-              ...aggregatePrefecture,
-              isChecked: e.target.checked,
-            };
-          }
-          return aggregatePrefecture;
-        }),
-      );
+      setCheckboxStates({ ...checkboxStates, [prefCode]: e.target.checked });
     },
-    [aggregatePrefectures],
+    [checkboxStates],
   );
 
-  const [selectedPopulationCategory, setSelectedPopulationCategory] = useState<PopulationCategory>('総人口');
+  const [selectedPopulationLabel, setSelectedPopulationLabel] = useState<PopulationLabel>('総人口');
   const handleSelectChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
-    setSelectedPopulationCategory(e.target.value as PopulationCategory);
+    setSelectedPopulationLabel(e.target.value as PopulationLabel);
   }, []);
-
-  const data = useMemo(() => {
-    let result: {
-      [year: string]: {
-        [prefName: string]: number;
-      };
-    } = {};
-    for (const getPopulationByPrefCodeSuspenseQuery of getPopulationByPrefCodeSuspenseQueries) {
-      const selectedAggregatePrefecture = aggregatePrefectures.find(
-        (aggregatePrefecture) => aggregatePrefecture.prefCode === getPopulationByPrefCodeSuspenseQuery.data.prefCode,
-      );
-      if (!selectedAggregatePrefecture) {
-        continue;
-      }
-      const targetPopulations = getPopulationByPrefCodeSuspenseQuery.data.result.data
-        .filter(({ label }) => label === selectedPopulationCategory)
-        .flatMap(({ data: populations }) => {
-          return populations;
-        });
-
-      for (const targetPopulation of targetPopulations) {
-        if (targetPopulation.year <= getPopulationByPrefCodeSuspenseQuery.data.result.boundaryYear) {
-          const target =
-            result != null && result[targetPopulation.year] != null
-              ? {
-                  ...result[targetPopulation.year],
-                  [selectedAggregatePrefecture.prefName]: targetPopulation.value,
-                }
-              : {
-                  [selectedAggregatePrefecture.prefName]: targetPopulation.value,
-                };
-
-          result = { ...result, [targetPopulation.year]: target };
-        }
-      }
-    }
-    return Object.keys(result)
-      .map((key) => {
-        return result[key] != null
-          ? {
-              ...result[key],
-              year: Number(key),
-            }
-          : undefined;
-      })
-      .filter(Boolean) as { year: number; [key: string]: number }[];
-  }, [aggregatePrefectures, getPopulationByPrefCodeSuspenseQueries, selectedPopulationCategory]);
 
   return (
     <>
-      <PrefectureList aggregatePrefectures={aggregatePrefectures} onChecked={handleChecked} />
-      <select onChange={handleSelectChange} value={selectedPopulationCategory}>
+      <PrefectureList
+        prefectures={getPrefectureListQuery.data ?? []}
+        checkboxStates={checkboxStates}
+        onChecked={handleChecked}
+      />
+      <select onChange={handleSelectChange} value={selectedPopulationLabel}>
         <option value="総人口">総人口</option>
         <option value="年少人口">年少人口</option>
         <option value="生産年齢人口">生産年齢人口</option>
         <option value="老年人口">老年人口</option>
       </select>
-      <PrefectureCharts data={data} dataKeys={dataKeys} />
+      <Suspense fallback={null}>
+        <PrefectureCharts
+          getPrefectureListQuery={getPrefectureListQuery}
+          dataKeys={dataKeys}
+          selectedPrefCodes={selectedPrefCodes}
+          selectedPopulationLabel={selectedPopulationLabel}
+        />
+      </Suspense>
     </>
   );
 };
